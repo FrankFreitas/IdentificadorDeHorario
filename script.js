0c6db1ffae4aa94ec8d868ef57d10dda725cc56f
Frank Freitas <frankfreitas664@gmail.com>
2023-12-30 02:54:54 -0300

    identificador de horário

---
+++ script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = document.querySelector('div#msg')
+    var img = document.querySelector('#imagem')
+    var dd = new Date()
+    var hora = dd.getHours()
+    msg.innerHTML = `Agora são ${hora} horas `
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'img/manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        //boa tarde
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        //boa noite
+        img.src = 'img/noite.png'
+        document.body.style.background = '#515154'
+    }
+}
